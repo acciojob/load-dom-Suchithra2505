6455bfee8b815f667771a2467ac50f49f9860f43
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-19 17:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+	var body = document.querySelector("body");
+	if (body.hasChildNodes()){
+		body.innerHTML = "";
+	}
+	 var st = document.createTextNode("DOM load success");
+	body.appendChild(st);
+});
+						  
